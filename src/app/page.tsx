badfc1019fcdea9a3653e80d692ae40acbe825db
Kate-Shu <k.onufrichuk@gmail.com>
2026-02-13 20:10:27 +0200

    chore: disable unfinished Workouts + Dance feature with coming soon state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,9 @@ const Home = () => {
       <section className="h-full flex flex-col pt-7 z-10">
         <p className="whitespace-nowrap ml-15 sm:ml-10 md:ml-20 lg:ml-30 xl:ml-40 mb-3 sm:mb-5 text-text-main font-sans font-bold leading-tight text-[clamp(1.5rem,5vw,3.25rem)] sm:text-[clamp(2rem,5vw,3.5rem)] lg:text-[3rem]">Your personal</p>
         <p className="ml-40 xs:ml-14 sm:ml-20 md:ml-30 lg:ml-40 xl:ml-50 mb-10 sm:mb-6 md:mb-9 lg:mb-11 xl:mb-15 text-text-main font-sans font-bold leading-tight text-[clamp(1.5rem,5vw,3.25rem)] sm:text-[clamp(2rem,5vw,3.5rem)] lg:text-[3rem] whitespace-nowrap">Fitness Coach</p>
-        <Button variant='primary' className='ml-15 sm:ml-30 md:ml-40 lg:ml-50 xl:ml-60 mb-10 sm:mb-6 md:mb-9 lg:mb-11 xl:mb-15 gradient-frst'>Workouts</Button>
-        <Button variant='primary' className='ml-15 sm:ml-40 md:ml-50 lg:ml-60 xl:ml-70 mb-10 sm:mb-6 md:mb-9 lg:mb-11 xl:mb-15 gradient-snd'>Dance</Button>
-        <Button variant='primary' onClick={() => setOpenOverlay(true)} className='ml-15 xs:ml-0 sm:ml-50 md:ml-60 lg:ml-70 xl:ml-80 gradient-thrd'>AI assistant</Button>
+        <Button variant='primary' onClick={() => setOpenOverlay(true)} className='ml-15 sm:ml-30 md:ml-40 lg:ml-50 xl:ml-60 mb-10 sm:mb-6 md:mb-9 lg:mb-11 xl:mb-15 gradient-frst'>AI ASSISTANT</Button>
+        <Button variant='primary' className='ml-15 sm:ml-40 md:ml-50 lg:ml-60 xl:ml-70 mb-10 sm:mb-6 md:mb-9 lg:mb-11 xl:mb-15 gradient-snd gradient-thrd opacity-50 cursor-not-allowed'>DANCE (Coming soon)</Button>
+        <Button variant='primary' className='ml-15 xs:ml-0 sm:ml-50 md:ml-60 lg:ml-70 xl:ml-80 gradient-thrd gradient-thrd opacity-50 cursor-not-allowed'>WORKOUTS (Coming soon)</Button>
       </section>
       <section className="hidden md:flex h-full min-h-full justify-center">
         <Image src='/main-no-bg.png' alt='main image' width={812} height={1490} sizes="100vh" className="w-auto h-[90vh] object-contain mb-17" >
--- src/components/ui/ButtonStyles.ts
+++ src/components/ui/ButtonStyles.ts
@@ -1,7 +1,7 @@
 export const baseStyles = 'relative inline-flex justify-center items-center cursor-pointer z-40 text-text-main'
 
 export const variants = {
- primary: "relative inline-block py-2 px-4 w-80 rounded-4xl border-gradient uppercase",
+ primary: "relative inline-block py-2 px-4 w-80 rounded-4xl border-gradient",
  submit: "w-10 h-10 rounded-full bg-bg-secondary",
  icon: "inset-0",
  text: "bg-transparent border-none cursor-pointer text-text-light font-serif uppercase"
